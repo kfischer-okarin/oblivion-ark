3caa859e8cc2beb2360da77694e95ff4d2639e21
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-02-24 22:50:36 +0900

    Extract module for ffi mock

--- test/objective_c_test.rb
+++ test/objective_c_test.rb
@@ -16,31 +16,40 @@ module ResetObjectiveCConstants
   end
 end
 
+module WithFFIMock
+  extend Minitest::Spec::DSL
+
+  let(:ffi) { Minitest::Mock.new }
+
+  before do
+    ObjectiveC.ffi = ffi
+  end
+
+  after do
+    ffi.verify
+  end
+end
+
 describe ObjectiveC do
   include ResetObjectiveCConstants
+  include WithFFIMock
 
   it 'can dynamically reference classes' do
     ns_string_address = 1234
-    ffi_mock = Minitest::Mock.new
-    ObjectiveC.ffi = ffi_mock
-    ffi_mock.expect :objc_getClass, ns_string_address, ['NSString']
+    ffi.expect :objc_getClass, ns_string_address, ['NSString']
 
     klass = ObjectiveC::NSString
 
     assert_equal ns_string_address, klass.address
-    ffi_mock.verify
   end
 
   it 'only loads the class once' do
     ns_string_address = 1234
-    ffi_mock = Minitest::Mock.new
-    ObjectiveC.ffi = ffi_mock
-    ffi_mock.expect :objc_getClass, ns_string_address, ['NSString']
+    ffi.expect :objc_getClass, ns_string_address, ['NSString']
 
     first_time = ObjectiveC::NSString
     second_time = ObjectiveC::NSString
 
     assert_equal second_time, first_time
-    ffi_mock.verify
   end
 end
