9068aae5745a1125f609cf8e2ff0b56c6902e480
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-03 23:46:39 +0900

    First object test

--- lib/objective_c/object.rb
+++ lib/objective_c/object.rb
@@ -2,5 +2,8 @@ require 'ffi'
 
 module ObjectiveC
   class Object < ::FFI::Pointer
+    def objc_class
+      @objc_class ||= ObjectiveC.ffi.object_getClass(self)
+    end
   end
 end
---
+++ test/objective_c/object_test.rb
@@ -0,0 +1,23 @@
+require 'minitest/autorun'
+
+require_relative '../test_helpers'
+require_relative '../../lib/objective_c'
+
+describe ObjectiveC::Object do
+  include ResetObjectiveCConstants
+  include WithFFIMock
+  include Factories
+
+  let(:obj) { ObjectiveC::Object.new(build_a_pointer) }
+
+  it 'can get its own class' do
+    class_pointer = build_a_pointer
+    ffi.expect :object_getClass, class_pointer, [obj]
+
+    result = obj.objc_class
+    result2 = obj.objc_class
+
+    assert_equal class_pointer, result
+    assert_equal class_pointer, result2
+  end
+end
