470f9acd40cf042c8a74109b7be7d5a1c785d161
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-05 22:41:03 +0900

    Initialize XCode project

---
+++ macos-ui/Mindhub/MindhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindhubApp.swift
+//  Mindhub
+//
+//  Created by Kevin Fischer on 2025/03/05.
+//
+
+import SwiftUI
+
+@main
+struct MindhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ macos-ui/MindhubTests/MindhubTests.swift
@@ -0,0 +1,17 @@
+//
+//  MindhubTests.swift
+//  MindhubTests
+//
+//  Created by Kevin Fischer on 2025/03/05.
+//
+
+import Testing
+@testable import Mindhub
+
+struct MindhubTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
