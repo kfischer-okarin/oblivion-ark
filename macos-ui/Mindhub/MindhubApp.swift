fc8b1b66391ad9d44ebbd217134bfc9b962c6072
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-08 02:36:07 +0900

    Show popup window with texteditor

--- macos-ui/Mindhub/MindhubApp.swift
+++ macos-ui/Mindhub/MindhubApp.swift
@@ -9,12 +9,42 @@ import SwiftUI
 
 @main
 struct MindhubApp: App {
+    @Environment(\.openWindow) private var openWindow
+
     var body: some Scene {
         MenuBarExtra {
+            Button("Quick Capture") {
+                openWindow(id: "quick-capture")
+            }
 
         } label: {
             Image(systemName: "brain.head.profile")
             Text("Mindhub")
+        }
+
+        Window("Capture Note", id: "quick-capture") {
+            QuickCaptureView()
+            .onAppear {
+                NSApp.activate(ignoringOtherApps: true)
+            }
+        }
+        .windowLevel(.floating)
+    }
+}
+
+struct QuickCaptureView: View {
+    @State private var note: String = ""
+    @FocusState private var isFocused: Bool
+
+    var body: some View {
+        VStack {
+            TextEditor(text: $note)
+                .frame(minWidth: 300, minHeight: 200)
+                .padding()
+                .focused($isFocused)
+        }
+        .onAppear() {
+            isFocused = true
         }
     }
 }
