f49cf348249e28ab3cf5112c84480a1472e4be5b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-27 02:08:50 +0900

    Fix mousedown handler
    Was broken after parametrizing with highlightStyle

--- frontend/markdownStyleDecorations.js
+++ frontend/markdownStyleDecorations.js
@@ -27,34 +27,16 @@ const markdownTheme = EditorView.theme({
 /**
  * Plugin that adds WYSIWYG-like styling to markdown elements
  */
-const markdownStyleDecorations = ({ highlightStyle }) => ViewPlugin.define(view => {
-  const urlColor = urlColorFrom(highlightStyle);
-
-  // Set the CSS variable on the editor element
-  if (urlColor) {
-    view.dom.style.setProperty('--url-color', urlColor);
-  }
-
-  const buildUrlMark = (url) => Decoration.mark({
-    attributes: {
-      'data-url': url,
-      class: 'cm-url-link'
-    }
-  });
-
-  const plugin = {
-    decorations: Decoration.none,
-  }
-
+const markdownStyleDecorations = ({ highlightStyle }) => {
   // Store link data to use when handling clicks
   let linkRanges = []
 
   // Track command key state
   let isCommandKeyPressed = false
 
-  function handleClick(event, view) {
+  function mousedown(event, view) {
     // Only process clicks when Command key is pressed
-    if (!event.metaKey) {
+    if (!isCommandKeyPressed) {
       return false;
     }
 
@@ -73,109 +55,126 @@ const markdownStyleDecorations = ({ highlightStyle }) => ViewPlugin.define(view 
     return true;
   }
 
-  function handleKeyDown(event) {
-    if (event.key === 'Meta') {
-      isCommandKeyPressed = true;
-      updateLinkInteractivity();
+  return ViewPlugin.define(view => {
+    const urlColor = urlColorFrom(highlightStyle);
+
+    // Set the CSS variable on the editor element
+    if (urlColor) {
+      view.dom.style.setProperty('--url-color', urlColor);
     }
-  }
 
-  function handleKeyUp(event) {
-    if (event.key === 'Meta') {
-      isCommandKeyPressed = false;
-      updateLinkInteractivity();
-    }
-  }
-
-  function updateLinkInteractivity() {
-    if (isCommandKeyPressed) {
-      view.dom.classList.add('cm-links-clickable');
-    } else {
-      view.dom.classList.remove('cm-links-clickable');
-    }
-  }
-
-  function update(view) {
-    linkRanges = [];
-
-    const decorationsArray = [];
-    const tree = syntaxTree(view.state);
-
-    tree.cursor().iterate(node => {
-      if (node.name.startsWith("ATXHeading")) {
-        const level = parseInt(node.name.substring(10)) || 1;
-        if (level >= 1 && level <= 6) {
-          const deco = Decoration.line({
-            class: `cm-heading cm-heading${level}`
-          });
-          const line = view.state.doc.lineAt(node.from);
-          decorationsArray.push(deco.range(line.from));
-        }
-      } else if (node.name === "Emphasis") { // *italic* or _italic_
-        const deco = Decoration.mark({
-          class: "cm-emphasis"
-        });
-        decorationsArray.push(deco.range(node.from, node.to));
-      } else if (node.name === "StrongEmphasis") { // **bold** or __bold__
-        const deco = Decoration.mark({
-          class: "cm-strong"
-        });
-        decorationsArray.push(deco.range(node.from, node.to));
-      } else if (node.name === "Strikethrough") { // ~~strikethrough~~
-        const deco = Decoration.mark({
-          class: "cm-strikethrough"
-        });
-        decorationsArray.push(deco.range(node.from, node.to));
-      } else if (node.name === "Link") {
-        const { textRange, url } = parseLinkNode(node.node, view);
-        if (!url) {
-          return;
-        }
-
-        const deco = buildUrlMark(url);
-        decorationsArray.push(deco.range(textRange.from, textRange.to));
-        linkRanges.push({ from: textRange.from, to: textRange.to, url });
-      } else if (node.name === "URL") {
-        // Skip if the node is already a link since we want to stress the link text and not the URL
-        if (node.matchContext(["Link"])) {
-          return;
-        }
-
-        const url = view.state.doc.sliceString(node.from, node.to);
-        const deco = buildUrlMark(url);
-        decorationsArray.push(deco.range(node.from, node.to));
-        linkRanges.push({ from: node.from, to: node.to, url });
+    const buildUrlMark = (url) => Decoration.mark({
+      attributes: {
+        'data-url': url,
+        class: 'cm-url-link'
       }
     });
 
-    plugin.decorations = Decoration.set(decorationsArray);
-  }
+    const plugin = {
+      decorations: Decoration.none,
+    }
 
-  plugin.update = update;
-  plugin.mousedown = handleClick;
+    function handleKeyDown(event) {
+      if (event.key === 'Meta') {
+        isCommandKeyPressed = true;
+        updateLinkInteractivity();
+      }
+    }
 
-  // Add global event listeners for command key detection
-  window.addEventListener('keydown', handleKeyDown);
-  window.addEventListener('keyup', handleKeyUp);
+    function handleKeyUp(event) {
+      if (event.key === 'Meta') {
+        isCommandKeyPressed = false;
+        updateLinkInteractivity();
+      }
+    }
 
-  // Clean up event listeners when plugin is destroyed
-  plugin.destroy = () => {
-    window.removeEventListener('keydown', handleKeyDown);
-    window.removeEventListener('keyup', handleKeyUp);
-  };
+    function updateLinkInteractivity() {
+      if (isCommandKeyPressed) {
+        view.dom.classList.add('cm-links-clickable');
+      } else {
+        view.dom.classList.remove('cm-links-clickable');
+      }
+    }
 
-  update(view);
+    function update(view) {
+      linkRanges = [];
 
-  return plugin;
-}, {
-  decorations: v => v.decorations,
-  eventHandlers: {
-    mousedown: (e, view) => {
-      return view.plugin(markdownStyleDecorations)?.mousedown(e, view) || false;
+      const decorationsArray = [];
+      const tree = syntaxTree(view.state);
+
+      tree.cursor().iterate(node => {
+        if (node.name.startsWith("ATXHeading")) {
+          const level = parseInt(node.name.substring(10)) || 1;
+          if (level >= 1 && level <= 6) {
+            const deco = Decoration.line({
+              class: `cm-heading cm-heading${level}`
+            });
+            const line = view.state.doc.lineAt(node.from);
+            decorationsArray.push(deco.range(line.from));
+          }
+        } else if (node.name === "Emphasis") { // *italic* or _italic_
+          const deco = Decoration.mark({
+            class: "cm-emphasis"
+          });
+          decorationsArray.push(deco.range(node.from, node.to));
+        } else if (node.name === "StrongEmphasis") { // **bold** or __bold__
+          const deco = Decoration.mark({
+            class: "cm-strong"
+          });
+          decorationsArray.push(deco.range(node.from, node.to));
+        } else if (node.name === "Strikethrough") { // ~~strikethrough~~
+          const deco = Decoration.mark({
+            class: "cm-strikethrough"
+          });
+          decorationsArray.push(deco.range(node.from, node.to));
+        } else if (node.name === "Link") {
+          const { textRange, url } = parseLinkNode(node.node, view);
+          if (!url) {
+            return;
+          }
+
+          const deco = buildUrlMark(url);
+          decorationsArray.push(deco.range(textRange.from, textRange.to));
+          linkRanges.push({ from: textRange.from, to: textRange.to, url });
+        } else if (node.name === "URL") {
+          // Skip if the node is already a link since we want to stress the link text and not the URL
+          if (node.matchContext(["Link"])) {
+            return;
+          }
+
+          const url = view.state.doc.sliceString(node.from, node.to);
+          const deco = buildUrlMark(url);
+          decorationsArray.push(deco.range(node.from, node.to));
+          linkRanges.push({ from: node.from, to: node.to, url });
+        }
+      });
+
+      plugin.decorations = Decoration.set(decorationsArray);
+    }
+
+    plugin.update = update;
+
+    // Add global event listeners for command key detection
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    // Clean up event listeners when plugin is destroyed
+    plugin.destroy = () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+
+    update(view);
+
+    return plugin;
+  }, {
+    decorations: v => v.decorations,
+    eventHandlers: {
+      mousedown,
     },
-  },
-  provide: () => markdownTheme,
-});
+    provide: () => markdownTheme,
+  });
+}
 
 function parseLinkNode(node, view) {
   const [
