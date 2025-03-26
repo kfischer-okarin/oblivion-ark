2b5f4471558063df0a739e78c2389c6c1b6031ba
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-03-26 22:49:50 +0900

    Format hyperlinks

--- frontend/markdownStyleDecorations.js
+++ frontend/markdownStyleDecorations.js
@@ -1,10 +1,20 @@
 import { ViewPlugin, Decoration } from '@codemirror/view';
 import { syntaxTree } from '@codemirror/language';
 
+import { oneDarkHighlightStyle } from '@codemirror/theme-one-dark';
+
 /**
  * Plugin that adds WYSIWYG-like styling to markdown elements
  */
 const markdownStyleDecorations = ViewPlugin.define(view => {
+  const highlightStyle = oneDarkHighlightStyle;
+  const urlColor = urlColorFrom(highlightStyle);
+  const buildUrlMark = (url) => Decoration.mark({
+    attributes: {
+      style: `text-decoration: underline; color: ${urlColor}; cursor: pointer;`,
+    }
+  });
+
   const plugin = {
     decorations: Decoration.none,
   }
@@ -38,6 +48,23 @@ const markdownStyleDecorations = ViewPlugin.define(view => {
           class: "cm-strikethrough"
         });
         decorationsArray.push(deco.range(node.from, node.to));
+      } else if (node.name === "Link") {
+        const { textRange, url } = parseLinkNode(node.node, view);
+        if (!url) {
+          return;
+        }
+
+        const deco = buildUrlMark(url);
+        decorationsArray.push(deco.range(textRange.from, textRange.to));
+      } else if (node.name === "URL") {
+        // Skip if the node is already a link since we want to stress the link text and not the URL
+        if (node.matchContext(["Link"])) {
+          return;
+        }
+
+        const url = view.state.doc.sliceString(node.from, node.to);
+        const deco = buildUrlMark(url);
+        decorationsArray.push(deco.range(node.from, node.to));
       }
     });
 
@@ -51,5 +78,44 @@ const markdownStyleDecorations = ViewPlugin.define(view => {
 }, {
   decorations: v => v.decorations
 });
+
+function parseLinkNode(node, view) {
+  const [
+    squareBracketLeft,
+    squareBracketRight,
+    bracketLeft,
+    bracketRight
+  ] = node.getChildren('LinkMark');
+
+  const textRange = { from: squareBracketLeft.to, to: squareBracketRight.from };
+  const text = view.state.doc.sliceString(textRange.from, textRange.to);
+
+  if (!bracketLeft || !bracketRight) {
+    return { text, textRange };
+  }
+
+  const urlRange = { from: bracketLeft.to, to: bracketRight.from };
+  const url = view.state.doc.sliceString(urlRange.from, urlRange.to);
+  return { text, url, textRange, urlRange };
+}
+
+const urlColorFrom = (highlightStyle) => {
+  const urlSpecs = specsApplyingToTag(highlightStyle, 'url');
+  if (urlSpecs.length === 0) {
+    return null;
+  }
+  const urlSpec = urlSpecs[0];
+  return urlSpec.color;
+}
+
+const specsApplyingToTag = (highlightStyle, tagName) => {
+  const specs = highlightStyle.specs;
+  return specs.filter(spec => {
+    if (Array.isArray(spec.tag)) {
+      return spec.tag.some(t => t.name === tagName);
+    }
+    return spec.tag.name === tagName;
+  });
+};
 
 export default markdownStyleDecorations;
