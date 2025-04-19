881c76b54bed8cc7e3ee910412e03f8c1665c9eb
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-04-20 01:37:56 +0900

    Refactor and comment typing logic

--- frontend/src/quick-capture-view-preload.js
+++ frontend/src/quick-capture-view-preload.js
@@ -11,36 +11,31 @@ contextBridge.exposeInMainWorld("electron", {
 ipcRenderer.on("enterText", (_, { text }) => {
   console.log("Received text to enter:", text);
 
-  simulateTypingInContentEditable(text);
+  if (isCodeMirrorEditor(document.activeElement)) {
+    simulateTypingInCodeMirrorEditor(text);
+  } else {
+    throw new Error("Don't know how to enter text in this element");
+  }
 });
 
-function simulateTypingInContentEditable(text) {
-  const selection = window.getSelection();
-  const range = selection.getRangeAt(0);
+function isCodeMirrorEditor(element) {
+  return element.classList.contains("cm-content");
+}
 
+function simulateTypingInCodeMirrorEditor(text) {
   let charIndex = 0;
   let lastCharWasNewline = false;
 
   function typeNextCharacter() {
+    // Need to move to next line container that was created by code mirror
+    // after entering a newline
     if (lastCharWasNewline) {
-      const nextLine = range.startContainer.nextSibling;
-      if (nextLine) {
-        range.setStart(nextLine, 0);
-        range.collapse(true);
-      }
+      moveCursorToStartOfNextSiblingElement();
     }
 
     const char = text.charAt(charIndex);
+    typeCharacter(char);
     lastCharWasNewline = char === "\n";
-    const textNode = document.createTextNode(char);
-    range.insertNode(textNode);
-    range.setStartAfter(textNode);
-    range.collapse(true);
-    selection.removeAllRanges();
-    selection.addRange(range);
-
-    const inputEvent = new Event("input", { bubbles: true });
-    document.activeElement.dispatchEvent(inputEvent);
 
     charIndex++;
 
@@ -51,4 +46,32 @@ function simulateTypingInContentEditable(text) {
   }
 
   typeNextCharacter();
+}
+
+function typeCharacter(char) {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+
+  const textNode = document.createTextNode(char);
+  range.insertNode(textNode);
+
+  // Update selection/caret position
+  range.setStartAfter(textNode);
+  range.collapse(true);
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Dispatch input event to notify any listeners
+  const inputEvent = new Event("input", { bubbles: true });
+  document.activeElement.dispatchEvent(inputEvent);
+}
+
+function moveCursorToStartOfNextSiblingElement() {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+  const nextLine = range.startContainer.nextSibling;
+  if (nextLine) {
+    range.setStart(nextLine, 0);
+    range.collapse(true);
+  }
 }
