69e55eeec8a8ab4cac08c0c60122f9ecc931a624
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-04-20 01:25:27 +0900

    Implement simulateTypingInContentEditable function

--- frontend/src/quick-capture-view-preload.js
+++ frontend/src/quick-capture-view-preload.js
@@ -7,3 +7,42 @@ import { WindowReadyEvent } from "../lib/rendererEvents.js";
 contextBridge.exposeInMainWorld("electron", {
   notifyWindowIsReady: () => WindowReadyEvent.send(ipcRenderer),
 });
+
+function simulateTypingInContentEditable(text) {
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+
+  let charIndex = 0;
+  let lastCharWasNewline = false;
+
+  function typeNextCharacter() {
+    if (lastCharWasNewline) {
+      const nextLine = range.startContainer.nextSibling;
+      if (nextLine) {
+        range.setStart(nextLine, 0);
+        range.collapse(true);
+      }
+    }
+
+    const char = text.charAt(charIndex);
+    lastCharWasNewline = char === "\n";
+    const textNode = document.createTextNode(char);
+    range.insertNode(textNode);
+    range.setStartAfter(textNode);
+    range.collapse(true);
+    selection.removeAllRanges();
+    selection.addRange(range);
+
+    const inputEvent = new Event("input", { bubbles: true });
+    document.activeElement.dispatchEvent(inputEvent);
+
+    charIndex++;
+
+    if (charIndex < text.length) {
+      const typingDelay = 20 + Math.floor(Math.random() * 20);
+      setTimeout(typeNextCharacter, typingDelay);
+    }
+  }
+
+  typeNextCharacter();
+}
