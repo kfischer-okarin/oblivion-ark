cad82ef2a1523e408c3ed0c52e8dfcec36832788
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-04-20 13:14:13 +0900

    Refactor preload args deconstruction

--- frontend/src/quick-capture-view-preload.js
+++ frontend/src/quick-capture-view-preload.js
@@ -5,12 +5,13 @@ const { contextBridge, ipcRenderer } = require("electron/renderer");
 import { WindowReadyEvent } from "../lib/rendererEvents.js";
 import { defineDriverCommandHandlers } from "./driverCommands.js";
 
+const { driverActive } = JSON.parse(process.argv[process.argv.length - 1]);
+
 contextBridge.exposeInMainWorld("electron", {
   notifyWindowIsReady: () => WindowReadyEvent.send(ipcRenderer),
 });
 
-const argFromMainProcess = JSON.parse(process.argv[process.argv.length - 1]);
-if (argFromMainProcess.driverActive) {
+if (driverActive) {
   console.log("Connected to driver socket");
   defineDriverCommandHandlers(ipcRenderer);
 }
