bfd2e085bca4dab3fa9d306a3d47f548680dba20
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2025-04-26 23:50:20 +0900

    Inline Driver class into AcceptanceTest + DSL module

--- acceptance-tests/acceptance_test_dsl.rb
+++ acceptance-tests/acceptance_test_dsl.rb
@@ -1,14 +1,16 @@
 module AcceptanceTestDSL
   def start_capture_note
-    @driver.start_capture_note
+    @driver_client.trigger_global_shortcut(accelerator: 'Shift+F5')
+    @driver_client.wait_for_window_shown # TODO: Match page
   end
 
   def enter_note_text(text)
-    @driver.enter_note_text(text)
+    @driver_client.enter_text(text:)
+    @driver_client.wait_for_enter_text_done
   end
 
   def submit_note
-    @driver.submit_note
+    @driver_client.send_key(key: 'Cmd+Enter')
   end
 
   def method_missing(name, *args, &block)
--- acceptance-tests/test_helper.rb
+++ acceptance-tests/test_helper.rb
@@ -2,18 +2,35 @@ require 'minitest/autorun'
 require 'minitest/reporters'
 
 require_relative 'acceptance_test_dsl'
-require_relative 'electron_app_socket_driver'
+require_relative 'lib/app'
 
 Minitest::Reporters.use! Minitest::Reporters::DefaultReporter.new
 
 class AcceptanceTest < Minitest::Test
   include AcceptanceTestDSL
 
+  class << self
+    def app
+      return @app if @app
+
+      App.build
+      @app = App.start
+
+      at_exit do
+        puts 'Shutting down Electron app...'
+        @app.shutdown
+        puts 'Electron app shut down.'
+      end
+
+      @app
+    end
+  end
+
   def before_setup
-    @driver = ElectronAppSocketDriver.new
+    @driver_client = self.class.app.driver_client
   end
 
   def after_teardown
-    @driver.teardown
+    @driver_client.reset_application
   end
 end
